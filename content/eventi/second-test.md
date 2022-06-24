2a8465a9c584ee0a972717c240e72e616f36c1b3
samoorai <hirotakatech@gmail.com>
2022-06-24 12:49:27 +0200

    Aggiunta sezione eventi

---
+++ content/eventi/second-test.md
@@ -0,0 +1,16 @@
+---
+title: Second post
+description: 'Empower your NuxtJS application with @nuxt/content module: write in a content/ directory and fetch your Markdown, JSON, YAML and CSV files through a MongoDB like API, acting as a Git-based Headless CMS.'
+---
+
+## Second this
+
+This is a second post
+
+## Fetching content
+
+Learn how to fetch your content with `$content`: https://content.nuxtjs.org/fetching.
+
+## Displaying content
+
+Learn how to display your Markdown content with the `<nuxt-content>` component directly in your template: https://content.nuxtjs.org/displaying.
--- pages/eventi.vue
+++ pages/eventi.vue
@@ -1,26 +1,37 @@
 <template>
-<div>
-  <!-- <div v-for="evento in eventi" :key="evento.title">
+  <div>
+    <!-- <div v-for="evento in eventi" :key="evento.title">
       <nuxt-content  :document="evento" />
 </div> -->
-  <div>
-    <h1 class="text-7xl font-black text-center">Eventi</h1>
-    <div v-for="evento in eventi" :key="evento.title">
-      <h2 class="mt-2 text-3xl font-semibold">{{ evento.title }}</h2>
+    <div>
+      <main role="main" class="container">
+        <div class="row">
+          <div class="col-12 blog-main">
+            <h3 class="pb-3 mb-4 font-italic border-bottom">Eventi</h3>
+            <div v-for="evento in eventi" :key="evento.title" class="border-bottom">
+              <div class="blog-post">
+                <h2 class="blog-post-title">{{ evento.title }}</h2>
+                <p class="blog-post-meta">
+                  January 1, 2014 by <a href="#">Mark</a>
+                </p>
+               <nuxt-content :document="evento" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-  export default {
-    async asyncData({ $content, params }) {
-      const eventi = await $content('eventi')
-      .only(['title', 'image', 'tags', 'slug'])
+export default {
+  async asyncData({ $content, params }) {
+    const eventi = await $content('eventi')
       .sortBy('createdAt', 'desc')
       .fetch()
 
-      return {eventi}
-    }
-  }
+    return { eventi }
+  },
+}
 </script>
